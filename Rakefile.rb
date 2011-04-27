2767fa13c15b2c2afb4ae4e32dac56a602d22e56
Amir Barylko <amir@barylko.com>
2011-04-27 02:17:11 -0500

    Changed nunit version, added test for listing an empty collection

--- Rakefile.rb
+++ Rakefile.rb
@@ -65,7 +65,7 @@ namespace :test do
 
 	desc "Run acceptance tests"
 	nunit :unit => ["build:all"] do |nunit|
-		nunit.command = "packages/NUnit.2.5.10.11092/Tools/nunit-console.exe"
+		nunit.command = "packages/NUnit.2.5.9.10348/Tools/nunit-console.exe"
 		nunit.assemblies FileList["test/unit/**/bin/debug/*Tests.dll"]
 	end
 
---
+++ test/unit/MovieLibrary.Website.Tests/Controllers/When_movies_controller_lists_no_movies.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using MovieLibrary.Core;
+using MavenThought.Commons.Testing;
+using MvcContrib.TestHelper;
+using SharpTestsEx;
+
+namespace MovieLibrary.Website.Tests.Controllers
+{
+    /// <summary>
+    /// Specification when listing no movies
+    /// </summary>
+    [Specification]
+    public class When_movies_controller_lists_no_movies : MoviesControllerSpecification
+    {
+        /// <summary>
+        /// Checks that no movies where returned
+        /// </summary>
+        [It]
+        public void Should_not_return_any_movies()
+        {
+            var result = (ViewResult) this.ActualResult;
+
+            result.ViewData.Cast<IEnumerable<IMovie>>().Should().Be.Empty();
+        }
+
+        /// <summary>
+        /// Checks that the expected view is rendered
+        /// </summary>
+        [It]
+        public void Should_render_the_index_view()
+        {
+            this.ActualResult.AssertViewRendered();
+        }
+
+        protected override void WhenIRun()
+        {
+            this.ActualResult = this.Sut.Index();
+        }
+    }
+}
